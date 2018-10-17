542d75267fe8662de6d81806977d53d48b62b3db
Arvind Iyer <arvindi.me@gmail.com>
2018-10-17 11:06:55 +0900

    WIP: Merging envdataclasses and envattrs

--- src/envattrs/__init__.py
+++ src/envattrs/__init__.py
@@ -1,17 +1,35 @@
 import os
 from typing import TypeVar, Type
 
-import attr
+try:
+    import attr
+except ImportError:
+    try:
+        import dataclasses
+    except ImportError:
+        print("Neither dataclasses not attr libraries available")
+        print("Make sure you are using either one and install that module")
 
-T = TypeVar('T')
+
+T = TypeVar("T")
 
 SubAttrs = object()
 
 
 def _iter(cls, prefix, source):
-    for field in attr.fields(cls):
+    # Check if it is an attr class or dataclass
+    # and use the corresponding fields function
+    if attr.has(cls):
+        fields = attr.fields
+    elif dataclasses.is_dataclass(cls):
+        fields = dataclasses.fields
+    else:
+        print("Object is neither dataclass nor attr class")
+        return
+
+    for field in fields(cls):
         sub = field.metadata.get(SubAttrs, None)
-        key = f'{prefix.upper()}_{field.name.upper()}'
+        key = f"{prefix.upper()}_{field.name.upper()}"
         if sub is None:
             try:
                 value = source[key]
