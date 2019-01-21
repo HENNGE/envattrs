c7628f85ee650416b853a30b6560b652c9fc99fe
Dima Tisnek <dimaqq@gmail.com>
2019-01-21 18:23:21 +0900

    calver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    version="1.2.0",
+    version="19.1",
     name="envattrs",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
