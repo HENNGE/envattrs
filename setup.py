c910ebe8008fb38df3231b555ecb274d7e759a5b
Dima Tisnek <dimaqq@gmail.com>
2019-01-21 18:21:00 +0900

    black

--- setup.py
+++ setup.py
@@ -2,19 +2,17 @@ from setuptools import setup, find_packages
 
 
 setup(
-    version='1.2.0',
-    name='envattrs',
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-    install_requires=[
-        'attrs>=17.1.0',
-    ],
-    license='APLv2',
+    version="1.2.0",
+    name="envattrs",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=["attrs>=17.1.0"],
+    license="APLv2",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ]
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
