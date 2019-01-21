b617b4bb6bd77a1f39efea00f77dc72934721de9
Dima Tisnek <dimaqq@gmail.com>
2019-01-21 18:22:23 +0900

    Newer attrs

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="envattrs",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=["attrs>=17.1.0"],
+    install_requires=["attrs>=17.4.0"],
     license="APLv2",
     classifiers=[
         "Development Status :: 3 - Alpha",
